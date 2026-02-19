6a428146f365c9730ae81abd9eba11296f4e6783
nicolasgodoy <nicolasgodoy540@gmail.com>
2026-02-19 15:25:59 -0300

    subiendo cambios

--- client/src/components/ExpenseModal.jsx
+++ client/src/components/ExpenseModal.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { X } from 'lucide-react';
 
 function ExpenseModal({ isOpen, onClose, onSave, initialData }) {
